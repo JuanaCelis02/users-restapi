f503a29247921f95a09aca4dbcb2c7c2ca4b7a74
JuanaCelis02 <juana.celis@uptc.edu.co>
2023-09-12 12:29:52 -0500

    UserRole entity added

--- src/models/Role.js
+++ src/models/Role.js
@@ -1,7 +1,7 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../database/database.js";
 
-export const Role = sequelize.define('role', {
+export const Role = sequelize.define('roles', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
